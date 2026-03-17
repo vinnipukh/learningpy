6bede7aad05b1ac808ae7e3c73c4e783dc8a8f54
vinnipukh <oyunlarinefendiler@gmail.com>
2026-03-17 19:34:48 +0300

    titanic

--- brocode/titanic.py
+++ brocode/titanic.py
@@ -1,10 +1,34 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("titanic.csv")
 df = df.drop(columns=["PassengerId","Name","Ticket","Cabin"])
 print(df["Age"].isnull().sum())
+
+"""
+#plotting part
+
+age_counts = df['Age'].value_counts()
+plt.style.use("dark_background")
+plt.figure(figsize=(10,10))
+plt.bar(age_counts.index, age_counts.values,color="Aqua")
+plt.title("Age Distribution")
+plt.xlabel("Age",fontsize=10)
+plt.ylabel("Age Count(how many people are x years old)",fontsize=10)
+plt.show()
+"""
+
+#training part
 df["Age"] = df["Age"].replace(np.nan,df["Age"].median())
-print(df.info())
+df.info()
+
+
+
+X_train,X_test,y_train,y_test = train_test_split()
+
+
 
 
